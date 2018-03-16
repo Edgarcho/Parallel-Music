a1a5b6cef58b24f7dc95dc9294c1ce976caef2fc
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-16 12:01:23 -0700

    Add intent to main activity class

--- app/src/main/java/com/epicodus/lyricsmatcher/MainActivity.java
+++ app/src/main/java/com/epicodus/lyricsmatcher/MainActivity.java
@@ -1,21 +1,34 @@
 package com.epicodus.lyricsmatcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 @BindView(R.id.searchButton) Button mSearchButton;
 @BindView(R.id.appName) TextView mAppName;
-
-
+@BindView(R.id.songEditText) EditText mSongEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String song = mSongEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this,TrackSearchActivity.class);
+                intent.putExtra("song",song)
+                startActivity(intent);
+            }
+        });
     }
 }
