f6f426955b8a7e5d242ae841b6e5cfa232117656
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-16 11:44:09 -0700

    Create about, most popular, track search activity

--- app/src/main/java/com/epicodus/lyricsmatcher/MainActivity.java
+++ app/src/main/java/com/epicodus/lyricsmatcher/MainActivity.java
@@ -2,8 +2,16 @@ package com.epicodus.lyricsmatcher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+@BindView(R.id.searchButton) Button mSearchButton;
+@BindView(R.id.appName) TextView mAppName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
