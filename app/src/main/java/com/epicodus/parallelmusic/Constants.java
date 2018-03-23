ca4f44aed0766d13611da83c19d5b43b597798b5
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-23 11:15:23 -0700

    Create base url and query parameters in Constants file

--- app/src/main/java/com/epicodus/parallelmusic/Constants.java
+++ app/src/main/java/com/epicodus/parallelmusic/Constants.java
@@ -6,4 +6,7 @@ package com.epicodus.parallelmusic;
 
 public class Constants {
     public static final String LASTFM_TOKEN  = BuildConfig.LASTFM_TOKEN;
+    public static final String LASTFM_BASE_TRACKSEARCH_URL = "https://ws.audioscrobbler.com/2.0/?method=track.search";
+    public static final String LASTFM_SONG_QUERY_PARAMETER = "song";
+    public static final String LASTFM_FORMAT_PARAMETER = "json";
 }
