f3b83da0b6fa0b37a7040551d5bcfe0e31718819
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-01 16:25:17 -0700

    Add TrackPagerAdapter to TrackDetailActivity

--- app/src/main/java/com/epicodus/parallelmusic/ui/TrackDetailActivity.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/TrackDetailActivity.java
@@ -1,15 +1,33 @@
 package com.epicodus.parallelmusic.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.parallelmusic.R;
+import com.epicodus.parallelmusic.adapters.TrackPagerAdapter;
+
+import org.parceler.Parcel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TrackDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private TrackPagerAdapter adapterViewPager;
+    ArrayList<Track> mTracks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_detail);
+        ButterKnife.bind(this);
+
+        mTracks = Parcel.unwrap(getIntent().getParcelableArrayExtra("tracks"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new TrackPagerAdapter(getSupportFragmentManager(), mTracks);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
