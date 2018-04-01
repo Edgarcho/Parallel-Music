dafc47779064895f2b485e34dadee1f5e80403fb
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-01 15:38:44 -0700

    Bundle information with parceler

--- app/src/main/java/com/epicodus/parallelmusic/ui/TrackDetailFragment.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/TrackDetailFragment.java
@@ -13,16 +13,20 @@ import android.widget.TextView;
 
 import com.epicodus.parallelmusic.R;
 import com.epicodus.parallelmusic.models.Track;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TrackDetailFragment extends Fragment {
-    @BindView(R.id.trackDetailImageView) ImageView mTrackDetail;
+    @BindView(R.id.trackDetailImageView) ImageView mTrackImage;
+    @BindView(R.id.trackNameTextView) TextView mTrackName;
     @BindView(R.id.trackArtistDetailTextView) TextView mTrackArtist;
     @BindView(R.id.listenerTextView) TextView mTrackListener;
     @BindView(R.id.webTextView) TextView mWebsite;
@@ -38,12 +42,26 @@ public class TrackDetailFragment extends Fragment {
         return trackDetailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mTrack = Parcels.unwrap(getArguments().getParcelable("track"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_track_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mTrack.getImageUrl()).into(mTrackImage);
+
+        mTrackName.setText(mTrack.getName());
+        mTrackArtist.setText(mTrack.getArtist());
+        mTrackListener.setText(Double.toString(mTrack.getListeners()));
+        mWebsite.setText(mTrack.getWebsite());
+
+        return view;
     }
 
 }
