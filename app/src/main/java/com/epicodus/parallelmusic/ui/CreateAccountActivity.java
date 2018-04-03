7ef544d8a1ede676fad8f6256eaf67073a49f624
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-03 14:33:59 -0700

    Create new activity and layout to let user create an account

--- app/src/main/java/com/epicodus/parallelmusic/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/CreateAccountActivity.java
@@ -3,13 +3,11 @@ package com.epicodus.parallelmusic.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.epicodus.parallelmusic.R;
-
-public class AboutActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_create_account);
     }
 }
