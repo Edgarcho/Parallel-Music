8e31950a76e4691dd87c53912bc211fdc83ad172
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-04 14:01:30 -0700

    Create onCreateOptionsmenu method in MainActivity

--- app/src/main/java/com/epicodus/parallelmusic/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,6 +65,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mSearchTrackButton.setOnClickListener(this);
         mSavedTrackButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
