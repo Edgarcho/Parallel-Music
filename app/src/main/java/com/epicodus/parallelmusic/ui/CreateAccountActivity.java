3b7069f86af475b398dfe59f7bf1b1df247821d9
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-04 15:12:20 -0700

    Add progress dialog to createAccountActivity

--- app/src/main/java/com/epicodus/parallelmusic/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.parallelmusic.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -34,6 +35,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         mAuth = FirebaseAuth.getInstance();
         createAuthStateListener();
+        createAuthProgressDialog();
 
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
@@ -86,10 +89,16 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         boolean validPassword = isValidPassword(password, confirmPassword);
         if (!validEmail || !validName || !validPassword) return;
 
+        mAuthProgressDialog.show();
+
+
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
+                mAuthProgressDialog.dismiss();
+
                 if (task.isSuccessful()){
                     Log.d(TAG, "Authentication successful");
                 }else{
@@ -115,6 +124,14 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         };
 
     }
+
+    private void createAuthProgressDialog(){
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
     private boolean isValidEmail(String email){
         boolean isGoodEmail = (email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches());
         if(!isGoodEmail){
