60f43f4d482068b142f787768df460b7b2f8bb75
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-05 11:41:23 -0700

    Add progress dialog and set on click method for login button

--- app/src/main/java/com/epicodus/parallelmusic/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/parallelmusic/ui/LoginActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.parallelmusic.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -68,12 +73,50 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    private void loginWithPassword() {
+        String email = mLoginEmailEditText.getText().toString().trim();
+        String password = mLoginPasswordEditText.getText().toString().trim();
+
+        if (email.equals("")){
+            mLoginEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if(password.equals("")){
+            mLoginPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+
+        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mAuthProgressDialog.dismiss();
+
+                if(!task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+
     @Override
     public void onClick(View view) {
-        if(view == mRegisterTextView){
+        if (view == mRegisterTextView){
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
             finish();
+        }
+        if (view == mLoginButton){
+            loginWithPassword();
         }
     }
 }
