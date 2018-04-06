4d25d93c8104d480e8b7003c1f5c207f97211b20
Edgar Hernandez <edgarchoritiz@.com>
2018-04-06 10:04:23 -0700

    Create new layout with reorder imagview

--- app/src/main/java/com/epicodus/parallelmusic/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/epicodus/parallelmusic/util/SimpleItemTouchHelperCallback.java
@@ -1,8 +1,44 @@
 package com.epicodus.parallelmusic.util;
 
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
 /**
  * Created by Guest on 4/6/18.
  */
 
-public class SimpleItemTouchHelperCallback {
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
+    private final ItemTouchHelperAdapter mAdapter;
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter){
+        mAdapter = adapter;
+    }
+    @Override
+    public boolean isLongPressDragEnabled(){
+        return true;
+    }
+    @Override
+    public boolean isItemViewSwipeEnabled(){
+        return true;
+    }
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        if (source.getItemViewType() != target.getItemViewType()) {
+            return false;
+        }
+        mAdapter.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        mAdapter.onItemMove(viewHolder.getAdapterPosition());
+
+    }
 }
